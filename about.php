fcaf599632cd7895c65c2eaf07f0ff32746bcfd2
Shivangi Singh <143539699+shivangisingh24@users.noreply.github.com>
2024-05-07 13:07:08 +0530

    Add files via upload
    This is a E-commerce Website By using language php.html, sql, css

---
+++ about.php
@@ -0,0 +1,106 @@
+<?php
+include 'components/connect.php';
+session_start();
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About</title>
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<?php include 'components/user_header.php'; ?>
+<section class="about">
+   <div class="row">
+      <div class="image">
+         <img src="images/23.png" alt="">
+      </div>
+      <div class="content">
+         <h3>Welcome to Onway.Com:</h3>
+         <p>Hey There ! We are here to provide service of e commerce where you can purchase our products sitting at your home and feel free to contact us on our helpline number.</p>
+         <a href="contact.php" class="btn">Contact Us</a>
+      </div>
+   </div>
+</section>
+<section class="reviews">
+   <h1 class="heading">Client's Reviews.</h1>
+   <div class="swiper reviews-slider">
+   <div class="swiper-wrapper">
+      <div class="swiper-slide slide">
+         <img src="images/pic-5.jpg" alt="">
+         <p>Been using their services for quite a bit and have never had an issue with the quality of their products. Online e-products working great as well. Only issue I have is they usually deliver when I'm a little caught up, though I've set a preferred delivery time. Everything else has been good.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3> <a href="https://www.facebook.com/" target="_blank">Arti Sharma</a></h3>
+      </div>
+      <div class="swiper-slide slide">
+         <img src="images/pic-1.jpg" alt="">
+         <p>It is the first online services  which we can trust completely.I always unbox making a video and instantly complain if there's anything wrong. Sometimes even don't need to return the item and they process the refund. KinBech do heavy fine to sellers who send wrong products thats why its platform getting better day by day.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/" target="_blank">Vivek chaudhary</a></h3>
+      </div>
+      <div class="swiper-slide slide">
+         <img src="images/pic-3.jpg" alt="">
+         <p>onway is great if you choose good sellers . A variety of required item available . Customers can return and refund full amount within 7 days easily . KinBech is boosting eCommerce business in Kathmandu.It provides great opportunity to sale items online with ease.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/" target="_blank">Sunny chaudhary</a></h3>
+      </div>
+   </div>
+   <div class="swiper-pagination"></div>
+   </div>
+</section>
+<?php include 'components/footer.php'; ?>
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+<script>
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView:1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+</script>
+</body>
+</html>
